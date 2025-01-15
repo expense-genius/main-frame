2ca1d66720e717f52e1c216620f0ca952471b2ab
Pratim Sadhu <pratimsadhu@icloud.com>
2025-01-15 12:10:27 -0500

    update config variables

--- plaid/config.ts
+++ plaid/config.ts
@@ -1,5 +1,12 @@
-export const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID || '';
-export const PLAID_SECRET = process.env.PLAID_SECRET || '';
-export const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
-export const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions').split(',');
-export const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(',');
+import { Products } from "plaid";
+
+export const PLAID_CLIENT_NAME = process.env.PLAID_CLIENT_NAME || "";
+export const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID || "";
+export const PLAID_SECRET = process.env.PLAID_SECRET || "";
+export const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+export const PLAID_PRODUCTS = (
+  process.env.PLAID_PRODUCTS || Products.Transactions
+).split(",");
+export const PLAID_COUNTRY_CODES = (
+  process.env.PLAID_COUNTRY_CODES || "US"
+).split(",");
