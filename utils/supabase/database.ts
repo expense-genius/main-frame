49b6577bf75eaa5ab53589f23fdae5fddd6fc42a
Pratim Sadhu <pratimsadhu@icloud.com>
2025-01-17 21:49:35 -0500

    store accounts in database

--- utils/supabase/database.ts
+++ utils/supabase/database.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
+import { AccountBase, Item } from "plaid";
 
 /**
- * Store the Plaid item in the database
+ * Stores the Plaid item in the database
  * @param accessToken The access token for the Plaid item
  * @param itemId The item ID for the Plaid item
  * @param institutionId The institution ID for the Plaid item
@@ -30,4 +31,52 @@ export const storePlaidItem = async (
       },
     ]);
   });
+};
+
+/**
+ * Stores the Plaid accounts in the database
+ * @param accounts The accounts associated with the Plaid item
+ * @param item The Plaid item
+ * @returns
+ */
+export const storePlaidAccounts = async (
+  accounts: AccountBase[],
+  item: Item
+) => {
+  const supabase = await createClient();
+  const Item = item as any;
+  const id = "824c90eb-8acb-4322-8374-84a3d4bd3623";
+
+  // Fetch the authenticated user
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user?.id || id) {
+    throw new Error("User not found");
+  }
+
+  // Map accounts to the insert payload
+  const insertData = accounts.map((account) => ({
+    user_id: data.user.id,
+    item_id: item.item_id,
+    account_id: account.account_id,
+    available_balance: account.balances.available,
+    current_balance: account.balances.current,
+    currency: account.balances.iso_currency_code,
+    name: account.name,
+    official_name: account.official_name,
+    type: account.type,
+    subtype: account.subtype,
+    institution_id: Item.institution_id,
+    institution_name: Item.institution_name || "",
+  }));
+
+  // Perform batch insert
+  const { data: insertResult, error: insertError } = await supabase
+    .from("accounts")
+    .insert(insertData);
+
+  if (insertError) {
+    throw new Error(`Error inserting accounts: ${insertError.message}`);
+  }
+
+  return { message: "Successfully stored accounts", data: insertResult };
 };
