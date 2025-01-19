2426c1f395a11b1924c4742be33118024114ba2b
Pratim Sadhu <pratimsadhu@icloud.com>
2025-01-19 18:51:19 -0500

    create basic button component

---
+++ components/common/BasicButton.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+type ColorNames =
+  | "slate"
+  | "gray"
+  | "zinc"
+  | "neutral"
+  | "stone"
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "fuchsia"
+  | "pink"
+  | "rose";
+
+type ColorVariations =
+  | 50
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500
+  | 600
+  | 700
+  | 800
+  | 900
+  | 950;
+
+type BasicButtonProps = {
+  name: string;
+  color?: ColorNames;
+  variations?: ColorVariations;
+  dimensions?: { height: number; width: number };
+  zIndex?: number;
+  onClick: () => void;
+};
+
+const BasicButton: React.FC<BasicButtonProps> = ({
+  name,
+  color = "blue",
+  variations = 500,
+  dimensions = { height: 10, width: 28 },
+  zIndex = 50,
+  onClick,
+}) => {
+  // Dimensions
+  const height: number = dimensions.height;
+  const width: number = dimensions.width;
+
+  // Button class name
+  const buttonClass = `w-${width} h-${height} border-2 border-${color}-${variations} text-${color}-${variations} font-medium rounded-lg hover:bg-${color}-${variations} hover:text-white transition-colors duration-300 z-${zIndex}`;
+
+  return (
+    <motion.button
+      className={buttonClass}
+      whileTap={{ scale: 0.95 }}
+      onClick={onClick}
+    >
+      {name}
+    </motion.button>
+  );
+};
+
+export default BasicButton;
