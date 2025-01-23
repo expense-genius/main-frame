06437993285c3daf8da61d62368f0ac5231fd5ad
Pratim Sadhu <pratimsadhu@icloud.com>
2025-01-23 17:40:32 -0500

    add themes and customization

--- components/common/BasicButton.tsx
+++ components/common/BasicButton.tsx
@@ -38,6 +38,16 @@ type BasicButtonProps = {
 	zIndex?: number;
 	additionalClassName?: string;
 	onClick: () => void;
+	theme?: 'primary' | 'secondary' | 'danger';
+};
+
+const variantStyles = {
+	primary:
+		'w-28 h-10 border-2 border-blue-500 text-blue-500 font-medium rounded-lg hover:bg-blue-500 hover:text-white transition-colors duration-300 z-50',
+	secondary:
+		'w-28 h-10 border-2 border-gray-800 text-gray-800 font-medium rounded-lg hover:bg-gray-800 hover:text-white transition-colors duration-300 z-50',
+	danger:
+		'w-28 h-10 border-2 border-red-500 text-red-500 font-medium rounded-lg hover:bg-red-500 hover:text-white transition-colors duration-300 z-50',
 };
 
 const BasicButton: React.FC<BasicButtonProps> = ({
@@ -47,14 +57,35 @@ const BasicButton: React.FC<BasicButtonProps> = ({
 	dimensions = { height: 10, width: 28 },
 	zIndex = 50,
 	additionalClassName = '',
+	theme,
 	onClick,
 }) => {
 	// Dimensions
 	const height: number = dimensions.height;
 	const width: number = dimensions.width;
 
-	// Button class name
-	const buttonClass = `${additionalClassName} w-${width} h-${height} border-2 border-${color}-${variations} text-${color}-${variations} font-medium rounded-lg hover:bg-${color}-${variations} hover:text-white transition-colors duration-300 z-${zIndex}`;
+	let buttonClass = '';
+
+	// If theme is provided, use the variant styles
+	if (theme) {
+		buttonClass = clsx(variantStyles[theme], additionalClassName);
+	} else {
+		buttonClass = clsx(
+			`w-${width}`,
+			`h-${height}`,
+			'border-2',
+			`border-${color}-${variations}`,
+			`text-${color}-${variations}`,
+			'font-medium',
+			'rounded-lg',
+			`hover:bg-${color}-${variations}`,
+			'hover:text-white',
+			'transition-colors',
+			'duration-300',
+			`z-${zIndex}`,
+			additionalClassName
+		);
+	}
 
 	return (
 		<motion.button className={buttonClass} whileTap={{ scale: 0.95 }} onClick={onClick}>
